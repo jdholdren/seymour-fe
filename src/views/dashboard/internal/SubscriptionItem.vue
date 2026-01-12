8ba36dee3cc7cafd8be1c12bfaea68c429c155df
James Holdren <jamesdholdren@gmail.com>
2026-01-11 19:00:07 -0500

    clamp description and subscription names (#5)
    Fixes https://github.com/jdholdren/seymour/issues/7

--- src/views/dashboard/internal/SubscriptionItem.vue
+++ src/views/dashboard/internal/SubscriptionItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-auto bg-white rounded-md p-4 outline-1 outline-surface-container">
-    <div class="text-lg font-bold">{{ subscription.feed_name }}</div>
-    <div class="text-base">{{ subscription.feed_description }}</div>
+    <div class="text-lg font-bold line-clamp-1">{{ subscription.feed_name }}</div>
+    <div class="text-base line-clamp-3">{{ subscription.feed_description }}</div>
     <div class="text-base">Last synced: {{ subscription.last_synced }}</div>
   </div>
 </template>
